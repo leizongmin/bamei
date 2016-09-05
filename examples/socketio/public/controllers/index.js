b6ce22d96cd16b34670ff2970610646bc9713633
gyt-Macmini <igyteng@gmail.com>
2016-09-05 21:18:09 +0800

    bug fix

--- examples/socketio/public/controllers/index.js
+++ examples/socketio/public/controllers/index.js
@@ -9,16 +9,20 @@ app.controller('MainController', function ($scope) {
   };
   $scope.room = $stateParams.room;
 
-  var socket = io.connect('http://127.0.0.1:4000/' + $scope.room);
-  socket.on('chat', function (data) {
-    $scope.$apply(function () {
-      console.log(data);
-      data.time = new Date();
-      $scope.chat.message.push(data);
+  var socket;
+
+  var connectToRoom = function () {
+    socket = io.connect('http://127.0.0.1:4000/' + $scope.room);
+    socket.on('chat', function (data) {
+      $scope.$apply(function () {
+        data.time = new Date();
+        $scope.chat.message.push(data);
+      });
     });
-  });
+  };
 
   $scope.sendMessage = function () {
+    if(!socket) {return;}
     socket.emit('chat', $scope.chat.data);
     var data = {};
     data.data = angular.copy($scope.chat.data)
@@ -35,5 +39,13 @@ app.controller('MainController', function ($scope) {
   };
 
   var createRoom = io.connect('http://127.0.0.1:4000');
-  createRoom.emit('bamei', {room: $scope.room});
+  createRoom.on('connect', function() {
+    createRoom.emit('bamei', {room: $scope.room});
+    createRoom.on('bamei', function(data) {
+      if(data === 'success') {
+        socket && socket.disconnect && socket.disconnect();
+        connectToRoom();
+      }
+    });
+  });
 });
