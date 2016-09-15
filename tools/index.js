c02925865b5321568459c4b9809549a93b078896
Zongmin Lei <leizongmin@gmail.com>
2016-09-15 16:49:57 +0800

    style: 代码风格

--- tools/index.js
+++ tools/index.js
@@ -10,47 +10,48 @@ const colors = require('colors');
 const inquirer = require('inquirer');
 
 if (process.argv[2]) {
-  return runCommand(process.argv[2]);
+  runCommand(process.argv[2]);
+} else {
+
+  const questions = [{
+    type: 'list',
+    name: 'command',
+    message: '选择要执行的命令',
+    choices: [
+      {
+        name: '创建新的模块',
+        value: 'new-module',
+      },
+      {
+        name: '更新模块文档',
+        value: 'update-docs',
+      },
+      {
+        name: '更新模块版本号',
+        value: 'update-version',
+      },
+      {
+        name: '发布所有模块',
+        value: 'publish-all',
+      },
+      {
+        name: '初始化开发环境（安装依赖模块）',
+        value: 'install-dependencies',
+      },
+      {
+        name: '退出 ^C',
+        value: 'exit',
+      },
+    ],
+  }];
+
+  inquirer.prompt(questions).then(answers => {
+    if (answers.command === 'exit') return;
+    runCommand(answers.command);
+  }).catch(err => {
+    console.error(colors.red(err.stack));
+  });
 }
-
-const questions = [{
-  type: 'list',
-  name: 'command',
-  message: '选择要执行的命令',
-  choices: [
-    {
-      name: '创建新的模块',
-      value: 'new-module',
-    },
-    {
-      name: '更新模块文档',
-      value: 'update-docs',
-    },
-    {
-      name: '更新模块版本号',
-      value: 'update-version',
-    },
-    {
-      name: '发布所有模块',
-      value: 'publish-all',
-    },
-    {
-      name: '初始化开发环境（安装依赖模块）',
-      value: 'install-dependencies',
-    },
-    {
-      name: '退出 ^C',
-      value: 'exit',
-    },
-  ],
-}];
-
-inquirer.prompt(questions).then(answers => {
-  if (answers.command === 'exit') return;
-  runCommand(answers.command);
-}).catch(err => {
-  console.error(colors.red(err.stack));
-});
 
 function runCommand(command) {
   require(`./commands/${ command }`);
